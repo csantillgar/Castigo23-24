578398a582b8b89842352a17dc8b50597489bab6
csantillgar <csantill@myuax.com>
2024-02-07 19:20:40 +0100

    guardar

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class ejercicio1 {
     public static void main(String[] args) {
         int diasEnUnAnio = 365;
         byte horasEnUnDia = 24;
---
+++ ejercicio2.java
@@ -0,0 +1,8 @@
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("El mayor número entero representable por un byte es: " + Byte.MAX_VALUE);
+        System.out.println("El mayor número entero representable por un short es: " + Short.MAX_VALUE);
+        System.out.println("El mayor número entero representable por un int es: " + Integer.MAX_VALUE);
+    }
+}
